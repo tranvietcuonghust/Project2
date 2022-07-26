2c0e46a8eed2ac39a15c9c77cec986181c51649d
Tran Viet Cuong <pioneer22022001@gmail.com>
2022-07-26 15:13:33 +0700

    Project2

---
+++ src/main/java/com/cuongtv/WebShop/Admin/GlobalData.java
@@ -0,0 +1,16 @@
+package com.cuongtv.WebShop.Admin;
+
+import com.cuongtv.WebShop.Product.ProductVar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalData {
+    public static List<ProductVar> productVars = new ArrayList<>();
+    static {
+        productVars = new ArrayList<>();
+    }
+
+
+
+}
